ae8185db794df39c032622196381d63f629a7d5c
AmeyaGurjar <amey992004@gmail.com>
2023-06-19 22:27:32 +0530

    Banana Veux: Nippon Included 13.0.9.0 Co-authored-by: Nippon <adamayyad1999@hotmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_veux.mk
+    $(LOCAL_DIR)/banana_veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_veux-user \
-    aosp_veux-userdebug \
-    aosp_veux-eng
+    banana_veux-user \
+    banana_veux-userdebug \
+    banana_veux-eng
