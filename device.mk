d2a88cb509a62fb0c7bb2b612ff3fe6a41c0d920
Omkar Chandorkar <gotenksIN@aospa.co>
2024-03-12 22:32:45 +0530

    veux: overlay: Configure Aperture camera
    - Also add overlay to unlock additional camera framerates
    This unlocks 60fps recording on main back camera (cam ID 0) and secondary back camera (cam ID 1). As for front facing camera (cam ID 1), it only supports 30fps recording on resolutions up to 1080p.
    
    [amrito1]: Adapt to sm6375-common
    
    Change-Id: I14e9bf9ba71c9d9f4aca04f3b558a1d72e248a60
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: amrito1 <bhabanidas431@gmail.com>

--- device.mk
+++ device.mk
@@ -332,7 +332,8 @@ PRODUCT_PACKAGES += \
     WifiOverlayS88007EA1 \
     WifiOverlayS88008BA1 \
     WifiOverlayS88106BA1 \
-    WifiOverlayS88107BA1
+    WifiOverlayS88107BA1 \
+    ApertureOverlay
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
